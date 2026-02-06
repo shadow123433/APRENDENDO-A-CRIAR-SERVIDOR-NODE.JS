f476387ee12563f6f7a279331441bb5d1a45ff82
shadow123433 <thiagoribeirodossantos965@gmail.com>
2026-02-06 10:18:50 -0300

    aprendendo conceitos basicos do node js

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
+   
+   
+   
+   
     //header
     res.setHeader('Content-Type', 'text/html');
-    res.write('<h1>Ola, mundo!</h1>');
-    res.end();
+    
+    //conteudo de html
+    fs.readFile('./html/pagina1.html', (err, data) => {
+        if (err) {
+            res.write('<h1>Erro ao carregar a página</h1>');
+        } else {
+            res.write(data);
+        }
+        res.end();
+    });
 });
+
+
+
+
+
 
 server.listen(3000, 'localhost', () => {
     console.log('Servidor rodando na porta 3000');
